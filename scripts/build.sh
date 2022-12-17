7889ab55584e6d108f173df99bdec3a890f8bf41
uturkarslan <ugurturkarslan@gmail.com>
2022-12-17 17:55:46 +0100

    Add build

---
+++ scripts/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Get commandline arguments
+while (( "$#" )); do
+  case "$1" in
+    --arm)
+      arm="true"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+if [[ $arm == "true" ]]; then
+  imageName="test-go-server-arm"
+  platform="linux/arm64"
+else
+  imageName="test-go-server-amd"
+  platform="linux/amd64"
+fi
+
+# Build image
+docker build \
+  --platform $platform \
+  --tag "${DOCKERHUB_NAME}/${imageName}" \
+  "../."
+
+# Push image to Docker Hub
+docker push "${DOCKERHUB_NAME}/${imageName}"
