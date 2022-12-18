91f17b21c3fcce3410eb791b94c7e10050003199
uturkarslan <ugurturkarslan@gmail.com>
2022-12-18 11:54:33 +0100

    Separate arm & amd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
-helm upgrade "server" \
-  --install \
-  --wait \
-  --debug \
-  --create-namespace \
-  --namespace "test" \
-  --set image.repository="uturkarslan/test-go-server-arm" \
-  --set image.tag="1.0.0" \
-  "../charts/server"
+# Get commandline arguments
+while (( "$#" )); do
+  case "$1" in
+    --arm)
+      arm="true"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+# ARM deployment
+if [[ $arm == "true" ]]; then
+  helm upgrade "server" \
+    --install \
+    --wait \
+    --debug \
+    --create-namespace \
+    --namespace "test" \
+    --set image.repository="uturkarslan/test-go-server-arm" \
+    --set image.tag="1.0.0" \
+    "../charts/server"
+
+# AMD deployment
+else
+  helm upgrade "server" \
+    --install \
+    --wait \
+    --debug \
+    --create-namespace \
+    --namespace "test" \
+    --set image.repository="uturkarslan/test-go-server-amd" \
+    --set image.tag="1.0.0" \
+    "../charts/server"
+fi
